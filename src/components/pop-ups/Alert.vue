e97d932efb6b39298d27833c9360d89fe9d0c00b
zgn <tw-zgn@foxmail.com>
2022-09-22 03:51:25 +0800

    封装 alert 和 confirm 组件

---
+++ src/components/pop-ups/Alert.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="wrapper">
+        <div class="alert">
+            <div class="wrap">
+                <div class="content" :message="message">{{ message }}</div>
+                <div class="echo">
+                    <div class="yes"><slot name="yes"></slot></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Alert",
+    props: ["message"],
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/confirm.scss";
+</style>
---
+++ src/components/pop-ups/Confirm.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="wrapper">
+        <div class="alert">
+            <div class="wrap">
+                <div class="content" :message="message">{{ message }}</div>
+                <div class="echo">
+                    <div class="yes"><slot name="yes"></slot></div>
+                    <div class="no"><slot name="no"></slot></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Confirm",
+    props: ["message"],
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/confirm.scss";
+</style>
