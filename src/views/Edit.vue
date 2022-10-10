8e8b7b828353ec78ab85a7f42e7191cb3ae175c8
zgn <tw-zgn@foxmail.com>
2022-10-11 05:53:09 +0800

    新增帮助文档

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -7,7 +7,7 @@
             <div>{{ "用户：" + username }}</div>
             <div>{{ "当前笔记：" + this.$route.params.note }}</div>
             <div>{{ conserve === "" ? "未保存" : "已保存：" + conserve }}</div>
-            <div>帮助</div>
+            <div @click="getHelp">帮助</div>
             <div @click="markdown">预览</div>
         </header>
         <div class="content">
@@ -17,6 +17,64 @@
                 @input="updateContent"
                 placeholder="请输入笔记内容,支持markdown语法"
             />
+            <div class="help" v-show="helpShow">
+                <h2>帮助文档</h2>
+                <hr />
+                <div><strong>注意：使用游客登录可以查看全功能教程</strong></div>
+                <div>
+                    <div class="title">一、强调</div>
+                    <em>*斜体*</em>
+                    <div><strong>**加粗**</strong></div>
+                    <em><strong>***既加粗又倾斜***</strong></em>
+                </div>
+                <div>
+                    <div class="title">二、表格和引用</div>
+                    <div>>表示引用</div>
+                    <div>>>支持多级引用</div>
+                    <div>>>>支持多级引用</div>
+                    <thead>
+                        <th>|支持|</th>
+                        <th>|创建|</th>
+                        <th>|表格|</th>
+                    </thead>
+                    <table>
+                        <td>使用</td>
+                        <td>中划线</td>
+                        <td>包裹</td>
+                    </table>
+                    <ol>
+                        <li>-表示项目符号</li>
+                    </ol>
+                </div>
+                <div>
+                    <div class="title">三、分割线语法</div>
+                    <div>多个空格可以添加行内代码</div>
+                    <div>-后不加内容表示分割线</div>
+                    <hr />
+                    <div>***分割线</div>
+                    <hr />
+                    <div>隔行表示分段</div>
+                </div>
+                <div>
+                    <div class="title">四、链接与代码块</div>
+                    <div>
+                        这是一个链接 [Markdown语法](https://markdown.com.cn)
+                    </div>
+                    <div>
+                        ~~~使用三个波浪线包裹的代码块，支持不同语言高亮显示~~~
+                    </div>
+                    <div>`反引号可以添加行内代码`</div>
+                </div>
+                <div>
+                    <div class="title">五、标题语法</div>
+                    <h1># 一级标题</h1>
+                    <h2>## 二级标题</h2>
+                    <h3>### 三级标题</h3>
+                    <h4>#### 四级标题</h4>
+                    <h5>###### 五级标题</h5>
+                    <h6>####### 六级标题</h6>
+                </div>
+            </div>
             <div class="isMarkdown" v-show="showMd" v-html="Markdown"></div>
         </div>
     </div>
@@ -68,6 +126,7 @@ export default {
             username: "",
             conserve: "",
             showMd: true,
+            helpShow: false,
         };
     },
     computed: {
@@ -96,8 +155,13 @@ export default {
             });
     },
     methods: {
+        getHelp() {
+            this.helpShow = !this.helpShow;
+            this.showMd = false;
+        },
         markdown() {
             this.showMd = !this.showMd;
+            this.helpShow = false;
         },
         updateContent: _.debounce(function () {
             notes
@@ -167,6 +231,24 @@ export default {
             margin-left: 10px;
             line-height: 2em;
             border: 1px solid rgb(189, 185, 185);
+        }
+        .help {
+            border: 1px solid rgb(189, 185, 185);
+            width: 800px;
+            height: 96vh;
+            padding: 50px;
+            margin-left: 10px;
+            line-height: 2em;
+            overflow: auto;
+            > div {
+                margin-top: 20px;
+                > .title {
+                    margin-bottom: 20px;
+                    padding: 5px;
+                    background: rgb(73, 79, 105);
+                    color: rgb(226, 226, 219);
+                }
+            }
         }
     }
 }
